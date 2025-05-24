b29b237bb73e35406decb67edf9af0fd128c3732
Erin <erinabduli7@gmail.com>
2025-05-24 21:21:07 +0200

    added login authentication

--- server/routes/products.js
+++ server/routes/products.js
@@ -5,12 +5,13 @@ import {
 	addProduct,
 	updateProduct,
 } from "../controllers/productController.js";
+import authenticate from "../auth/authenticate.js";
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
-router.post("/", addProduct);
-router.get("/:id", getProductById);
-router.put("/:id", updateProduct);
+router.post("/", authenticate, addProduct);
+router.get("/:id", authenticate, getProductById);
+router.put("/:id", authenticate, updateProduct);
 
 export default router;
